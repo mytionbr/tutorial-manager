c1095a951d3d7ff52f0e76eabef386cc128efebe
mytionBr <matheuss.jesus2017@gmail.com>
2022-01-23 13:13:21 -0300

    add prettier and eslint

--- dist/server.js
+++ dist/server.js
@@ -1,7 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = require("express");
-var helmet_1 = require("helmet");
 var routes_1 = require("./routes");
 var Server = /** @class */ (function () {
     function Server(app) {
@@ -9,7 +8,6 @@ var Server = /** @class */ (function () {
         new routes_1.default(app);
     }
     Server.prototype.config = function (app) {
-        app.use(helmet_1.default());
         app.use(express_1.urlencoded({ extended: true }));
         app.use(express_1.json());
     };
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,14 @@
 import { Application, json, urlencoded } from "express";
-import  helmet from 'helmet';
 import Router from "./routes";
 
-export default class Server{
-    constructor(app: Application){
-        this.config(app);
-        new Router(app);
-    }
+export default class Server {
+  constructor(app: Application) {
+    this.config(app);
+    new Router(app);
+  }
 
-    public config(app: Application): void{
-        app.use(helmet());
-        app.use(urlencoded({extended:true}));
-        app.use(json());
-    }
+  public config(app: Application): void {
+    app.use(urlencoded({ extended: true }));
+    app.use(json());
+  }
 }
